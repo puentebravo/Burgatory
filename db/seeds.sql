1da635b5d324a329904cd26c7d95a7c3aa9f17e3
Puentebravo <puentematos@gmail.com>
2021-02-13 22:29:09 -0500

    added seed file, inserted burgernames into table

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burgerName) VALUES ("Double Cheeseburger"), ("Bacon Bison Burger"), ("The Terminator - Ghost Pepper Burger with bacon and cheese")
+
+INSERT INTO burgers (burgerName) VALUES ("Rising Sun: Kobe Beef Burger on brioche")
