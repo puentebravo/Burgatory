28849bd1562c3984ff4bf76d0f9af3a29951bc30
Puentebravo <puentematos@gmail.com>
2021-02-13 23:45:18 -0500

    Configured connection, added dotenv for security

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "burgers_db",
+});
+
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+
+module.exports = connection;
