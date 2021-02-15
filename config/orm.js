9b0316de42d46dd3391959e0d665a5fde174ebfb
Puentebravo <puentematos@gmail.com>
2021-02-15 18:30:58 -0500

    Added selectAll function

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,12 @@
-const connection = require ("connection.js")
+const connection = require("connection.js");
 
 const orm = {
-    
-}
+  selectAll(cb) {
+    const query = "SELECT * FROM burgers;";
+    connection.query(query, (err, res) => {
+      if (err) throw err;
+      cb(res);
+    });
+  },
+  
+};
