f95f108f9ec641d5c7400c1f7dcdd1909dc625b0
Puentebravo <puentematos@gmail.com>
2021-02-15 20:15:32 -0500

    wrote burger model, module export statement

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,18 @@
 const orm = require("../config/orm");
 
+const burger = {
+  selectAll(cb) {
+    orm.selectAll((res) => cb(res));
+  },
+  insertOne(formInput, cb) {
+    orm.insertOne(formInput, (res) => cb(res));
+  },
+  updateOne(dataId, cb) {
+    orm.updateOne(dataId, (res) => cb(res));
+  },
+  deleteOne(dataId, cb) {
+    orm.deleteOne(dataId, (res) => cb(res));
+  },
+};
+
+module.exports = burger
