27f09676d6d6d7d2754fb906b4dabf649474d916
Puentebravo <puentematos@gmail.com>
2021-02-16 18:27:35 -0500

    Fixed post route, created put route to switch status to devoured

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,19 +1,26 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const burger = require("../models/burger")
+const burger = require("../models/burger");
 
 router.get("/", (req, res) => {
-    burger.selectAll((data) => {
-        res.render("index", data)
-    })
-})
+  burger.selectAll((data) => {
+    res.render("index", data);
+  });
+});
 
 router.post("/api/burgers", (req, res) => {
-    burger.insertOne((data) => {
-        burger.create(req.body.burgerName, (result) => {
-            res.json({id: result.id})
-        })
-    })
-})
+  burger.insertOne(req.body.burgerName, (result) => {
+    res.json({ id: result.id });
+  });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+  burger.updateOne(req.params.id, (result) => {
+    if (result.changedRows === 0) {
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  })
+});
