bee96746bbdbed529eca164fad52ed0462589feb
Puentebravo <puentematos@gmail.com>
2021-02-16 22:08:04 -0500

    Added JAWSDB connection code

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 const mysql = require('mysql');
 require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "burgers_db",
-});
+var connection ; 
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "burgers_db",
+  });
+}
+
+
 
 
 connection.connect((err) => {
